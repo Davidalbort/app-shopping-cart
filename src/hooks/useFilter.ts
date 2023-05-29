6d72ed758d84d90d8f0c7a211161ffed6c46e8d0
Roger David Alba Ortega <rogerdavidalbaortega@gmail.com>
2023-05-29 17:34:21 -0500

    Refactor: Show error when we want to use useFilter without provider

--- src/hooks/useFilter.ts
+++ src/hooks/useFilter.ts
@@ -3,8 +3,11 @@ import { FilterContext } from "../context/FilterContext"
 import { Product } from "../constants/type.td"
 
 export function useFilter() {
-  const value = useContext(FilterContext)
-  const { filters, updateCategory, updatePrice } = value
+  const context = useContext(FilterContext)
+  if (context === undefined) {
+    throw new Error("useFilter must be used within a FilterProvider")
+  }
+  const { filters, updateCategory, updatePrice } = context
   const filterProduct = (products: Product[]): Product[] => {
     return products.filter((product) => {
       return (
